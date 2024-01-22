e7601dd91e3d616d753d63cbae4295f1ec1b4ee1
3vil3y3 <101619460+evileye2002@users.noreply.github.com>
2024-01-22 17:14:45 +0700

    dynamic v0.1.6

--- src/lib/scraper/app.js
+++ src/lib/scraper/app.js
@@ -1,200 +1,202 @@
+const { sleep, convertToDate, removeDup } = require("../helper");
 const { default: puppeteer } = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const rua = require("random-useragent");
-const { sleep, convertToDate, removeDup } = require("../helper");
 const fs = require("fs").promises;
+
 puppeteer.use(StealthPlugin());
 
 //Fields
-const url = `https://mbasic.facebook.com/hashtag/motivation`;
 // const botCheck = "https://bot.sannysoft.com";
+const url = `https://mbasic.facebook.com/hashtag/motivation`;
 let urlJSON = {};
 let cookies = null;
 
-async function main(urlsImput, cookiesImput, fileOutput) {
-  const urlStrings = await fs.readFile(`./data/scraper/urls/${urlsImput}.json`);
-  urlJSON = JSON.parse(urlStrings);
+// async function main(urlsImput, cookiesImput, fileOutput) {
+//   const urlStrings = await fs.readFile(`./data/scraper/urls/${urlsImput}.json`);
+//   urlJSON = JSON.parse(urlStrings);
 
-  const browser = await puppeteer.launch({
-    headless: false,
-    // userDataDir:
-    //   "C:/Users/Admin/AppData/Local/Google/Chrome for Testing/User Data",
-  });
-  const page = await browser.newPage();
+//   const browser = await puppeteer.launch({
+//     headless: false,
+//     // userDataDir:
+//     //   "C:/Users/Admin/AppData/Local/Google/Chrome for Testing/User Data",
+//   });
+//   const page = await browser.newPage();
 
-  if (cookies === null) {
-    const cookiesString = await fs.readFile(
-      `./data/scraper/cookies/${cookiesImput}.json`
-    );
-    cookies = JSON.parse(cookiesString);
-  }
-  await page.setCookie(...cookies);
-  // await page.setUserAgent(rua.getRandom());
+//   if (cookies === null) {
+//     const cookiesString = await fs.readFile(
+//       `./data/scraper/cookies/${cookiesImput}.json`
+//     );
+//     cookies = JSON.parse(cookiesString);
+//   }
+//   await page.setCookie(...cookies);
+//   // await page.setUserAgent(rua.getRandom());
 
-  let allPosts = [];
-  for (let i = 0; i < urlJSON.length; i++) {
-    console.log("Url: " + i);
-    const url = urlJSON[i];
-    await page.goto(url);
+//   let allPosts = [];
+//   for (let i = 0; i < urlJSON.length; i++) {
+//     console.log("Url: " + i);
+//     const url = urlJSON[i];
+//     await page.goto(url);
 
-    // Scrape
-    const postsCurrentPage = await page.evaluate(() => {
-      const posts = document.querySelectorAll("div article");
+//     // Scrape
+//     const postsCurrentPage = await page.evaluate(() => {
+//       const posts = document.querySelectorAll("div article");
 
-      return Array.from(posts).map((post) => {
-        let creator = post.querySelector("header h3 a");
-        let creatorName = null;
-        let creatorID = null;
-        if (creator) {
-          creatorName = creator.innerText;
-          creatorID = creator.href
-            .split("eav=")[0]
-            .split("facebook.com/")[1]
-            .replace(/\?/g, "")
-            .replace(/&/g, "");
+//       return Array.from(posts).map((post) => {
+//         let creator = post.querySelector("header h3 a");
+//         let creatorName = null;
+//         let creatorID = null;
+//         if (creator) {
+//           creatorName = creator.innerText;
+//           creatorID = creator.href
+//             .split("eav=")[0]
+//             .split("facebook.com/")[1]
+//             .replace(/\?/g, "")
+//             .replace(/&/g, "");
 
-          if (creatorID.includes("__xts__"))
-            creatorID = creatorID.split("__xts__")[0];
-        }
+//           if (creatorID.includes("__xts__"))
+//             creatorID = creatorID.split("__xts__")[0];
+//         }
 
-        let divs = post.querySelectorAll("div[data-ft]");
-        let caption = divs[0] ? divs[0].innerText : null;
-        // let videoUrl = "";
-        // if (divs[1].querySelector("div div a") != null) {
-        //   videoUrl = divs[1].querySelector("div div a").href.split("&eav=")[0];
-        // }
+//         let divs = post.querySelectorAll("div[data-ft]");
+//         let caption = divs[0] ? divs[0].innerText : null;
+//         // let videoUrl = "";
+//         // if (divs[1].querySelector("div div a") != null) {
+//         //   videoUrl = divs[1].querySelector("div div a").href.split("&eav=")[0];
+//         // }
 
-        let likeElement = post.querySelector("footer span a");
-        let likes = likeElement ? likeElement.innerText : "0";
+//         let likeElement = post.querySelector("footer span a");
+//         let likes = likeElement ? likeElement.innerText : "0";
 
-        let footerElements = post.querySelectorAll("footer div a");
-        let cmts = "0";
-        let postUrl = null;
-        if (footerElements) {
-          footerElements.forEach((text) => {
-            if (text.innerText.includes("bình luận")) {
-              cmts = text.innerText.replace("bình luận", "").replace(" ", "");
-            }
+//         let footerElements = post.querySelectorAll("footer div a");
+//         let cmts = "0";
+//         let postUrl = null;
+//         if (footerElements) {
+//           footerElements.forEach((text) => {
+//             if (text.innerText.includes("bình luận")) {
+//               cmts = text.innerText.replace("bình luận", "").replace(" ", "");
+//             }
 
-            if (text.innerText.includes("Toàn bộ tin"))
-              postUrl = text.href.split("eav=")[0];
-          });
-        }
+//             if (text.innerText.includes("Toàn bộ tin"))
+//               postUrl = text.href.split("eav=")[0];
+//           });
+//         }
 
-        likes = Number(likes.replace(/\./g, ""));
-        cmts = Number(cmts.replace(/\./g, ""));
-        let dateElement = post.querySelector("footer > div > abbr");
-        let dateTime = dateElement ? dateElement.innerText : null;
+//         likes = Number(likes.replace(/\./g, ""));
+//         cmts = Number(cmts.replace(/\./g, ""));
+//         let dateElement = post.querySelector("footer > div > abbr");
+//         let dateTime = dateElement ? dateElement.innerText : null;
 
-        const id = postUrl.split("story_fbid=")[1];
-        // const urls = { postUrl, creatorUrl } ?? {};
+//         const id = postUrl.split("story_fbid=")[1];
+//         // const urls = { postUrl, creatorUrl } ?? {};
 
-        //Rating
-        let score = 0;
-        const rate = cmts / likes;
+//         //Rating
+//         let score = 0;
+//         const rate = cmts / likes;
 
-        const keys = [
-          "health",
-          "success",
-          "mind",
-          "inspiration",
-          "body",
-          "love",
-          "challenge",
-          "fitness",
-          "quote",
-          "workout",
-        ];
+//         const keys = [
+//           "health",
+//           "success",
+//           "mind",
+//           "inspiration",
+//           "body",
+//           "love",
+//           "challenge",
+//           "fitness",
+//           "quote",
+//           "workout",
+//         ];
 
-        switch (true) {
-          case likes >= 1000000:
-            score = 3;
-            break;
-          case likes >= 100000:
-            score = 2.5;
-            break;
-          case likes >= 10000:
-            score = 2;
-            break;
-          case likes >= 1000:
-            score = 1.5;
-            break;
-          case likes >= 100:
-            score = 1;
-            break;
-          case likes > 0:
-            score = 0.5;
-            break;
-          default:
-            score = 0;
-        }
+//         switch (true) {
+//           case likes >= 1000000:
+//             score = 3;
+//             break;
+//           case likes >= 100000:
+//             score = 2.5;
+//             break;
+//           case likes >= 10000:
+//             score = 2;
+//             break;
+//           case likes >= 1000:
+//             score = 1.5;
+//             break;
+//           case likes >= 100:
+//             score = 1;
+//             break;
+//           case likes > 0:
+//             score = 0.5;
+//             break;
+//           default:
+//             score = 0;
+//         }
 
-        switch (true) {
-          case rate >= 1:
-            score += 2.5;
-            break;
-          case rate >= 0.75:
-            score += 2;
-            break;
-          case rate >= 0.5:
-            score += 1.5;
-            break;
-          case rate >= 0.25:
-            score += 1;
-            break;
-          case rate > 0:
-            score += 0.5;
-            break;
-          default:
-            score += 0;
-        }
+//         switch (true) {
+//           case rate >= 1:
+//             score += 2.5;
+//             break;
+//           case rate >= 0.75:
+//             score += 2;
+//             break;
+//           case rate >= 0.5:
+//             score += 1.5;
+//             break;
+//           case rate >= 0.25:
+//             score += 1;
+//             break;
+//           case rate > 0:
+//             score += 0.5;
+//             break;
+//           default:
+//             score += 0;
+//         }
 
-        keys.forEach((key) => {
-          if (caption.includes(key)) {
-            score += 0.5;
-          }
-        });
+//         keys.forEach((key) => {
+//           if (caption.includes(key)) {
+//             score += 0.5;
+//           }
+//         });
 
-        return {
-          id,
-          creatorID,
-          creatorName,
-          caption,
-          likes,
-          cmts,
-          dateTime,
-          score,
-        };
-      });
-    });
+//         return {
+//           id,
+//           creatorID,
+//           creatorName,
+//           caption,
+//           likes,
+//           cmts,
+//           dateTime,
+//           score,
+//         };
+//       });
+//     });
 
-    if (postsCurrentPage.length > 0) {
-      postsCurrentPage.forEach((post) => {
-        if (post.dateTime) post.dateTime = convertToDate(post.dateTime);
-        allPosts.push(post);
-      });
-    } else {
-      console.log("BREAK At: " + i);
-      break;
-    }
+//     if (postsCurrentPage.length > 0) {
+//       postsCurrentPage.forEach((post) => {
+//         if (post.dateTime) post.dateTime = convertToDate(post.dateTime);
+//         allPosts.push(post);
+//       });
+//     } else {
+//       console.log("BREAK At: " + i);
+//       break;
+//     }
 
-    if (i < urlJSON.length - 1) await sleep(3000);
-  }
+//     if (i < urlJSON.length - 1) await sleep(3000);
+//   }
 
-  // console.log(allPosts);
-  // console.log("REMOVING DUP...");
-  // const finalPosts = removeDup(allPosts);
-  // console.log(`REMOVED ${allPosts.length - finalPosts.length} POSTS`);
+//   // console.log(allPosts);
+//   // console.log("REMOVING DUP...");
+//   // const finalPosts = removeDup(allPosts);
+//   // console.log(`REMOVED ${allPosts.length - finalPosts.length} POSTS`);
 
-  console.log("SAVING...");
-  await fs.writeFile(
-    `./data/scraper/${fileOutput}.json`,
-    JSON.stringify(allPosts, null, 2)
-  );
+//   console.log("SAVING...");
+//   await fs.writeFile(
+//     `./data/scraper/${fileOutput}.json`,
+//     JSON.stringify(allPosts, null, 2)
+//   );
 
-  console.log("DONE");
-  await browser.close();
-}
+//   console.log("DONE");
+//   await browser.close();
+// }
+// main("urls2", "cookies1", "posts-20");
 
 async function mainAuto(n, cookiesImput, fileOutput) {
   const browser = await puppeteer.launch({
@@ -398,5 +400,4 @@ async function mainAuto(n, cookiesImput, fileOutput) {
   await browser.close();
 }
 
-// main("urls2", "cookies1", "posts-20");
 mainAuto(500, 5, 35);
